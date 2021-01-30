cc35d346e4193e4e1360498da458b299b4f235fa
ForestGameMain <Forest_game_channele@mail.ru>
2021-01-30 22:20:40 +0300

    Update Betta Version Start menu. Russification and fix bugs, changing the interface.Appending new IMG, GIF and MP3 for next using. Worked music

--- main.py
+++ main.py
@@ -3,23 +3,34 @@ import pygame
 import pygame_menu
 
 pygame.init()
-surface = pygame.display.set_mode((600, 400))
+surface = pygame.display.set_mode((1280, 720))
+
 
 class StartPage():
     def set_difficulty(value, difficulty):
-        # Do the job here !
+        # Локация дано 1,2,3 можно потом где то применить
         pass
 
     def start_the_game():
         # Сюда писать поссылки на саму игру внутри или сам код
         pass
 
-    menu = pygame_menu.Menu(300, 400, 'Welcome', theme=pygame_menu.themes.THEME_BLUE)
+    def music_off_on(value, difficulty):
+        if value == 1:
+            pygame.mixer.unpause()
+        elif value == 0:
+            pygame.mixer.pause()
 
-    menu.add_text_input('Name :', default='John Doe')
-    menu.add_selector("Difficulty :", [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-    menu.add_button('Play', start_the_game)
-    menu.add_button('Quit', pygame_menu.events.EXIT)
+    menu = pygame_menu.Menu(720, 1280, 'Название игры', theme=pygame_menu.themes.THEME_DARK)
+    menu.add_selector("Музыка : ", [('Вкл', 1), ('Выкл', 0)], onchange=music_off_on)
+    menu.add_text_input('Ваше имя :', default='someone name')
+    menu.add_selector("Локация :", [('название карты 1', 1), ('название карты 2', 2), ('название карты 3', 2)],
+                      onchange=set_difficulty)
+    menu.add_button('Играть', start_the_game)
+    menu.add_button('Выйти', pygame_menu.events.EXIT)
+
+    song = pygame.mixer.Sound('Sounds/8bitlong.mp3')
+    song.play()
 
 
 if __name__ == '__main__':
