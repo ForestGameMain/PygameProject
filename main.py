b6bd4daea5c37fb75d345efa97796893f69e254b
ForestGameMain <Forest_game_channele@mail.ru>
2021-01-31 20:22:49 +0300

    Update Start Menu Alfa Version. All widgets on menu is working. Starting MainGame

--- main.py
+++ main.py
@@ -5,33 +5,40 @@ import pygame_menu
 pygame.init()
 surface = pygame.display.set_mode((1280, 720))
 song_start = pygame.mixer.Sound('Sounds/8bitlong.mp3')
+music_logic = 1
+
+class MainGame():
+    def start_class():
+        song_start.stop()
+        pygame.quit()
+        pygame.init()
+        if music_logic == 1:
+            song_start.play()
+        pygame.display.set_mode((1280, 720))
+        # сама игра
 
 
 class StartPage():
-    def set_difficulty(value, difficulty):
-        # Локация дано 1,2,3 можно потом где то применить
-        pass
-
-    def start_the_game():
-        print("START")
-        # Сюда писать поссылки на саму игру внутри или сам код
-        pass
-
     def music_play():
         song_start.play()
+        global music_logic
+        music_logic = 1
 
     def music_stop():
         song_start.stop()
+        global music_logic
+        music_logic = 0
+
+    def start_game():
+        MainGame.start_class()
 
     menu = pygame_menu.Menu(720, 1280, 'Arcanoid', theme=pygame_menu.themes.THEME_DARK)
     menu.add_button('Музыка Вкл', music_play)
     menu.add_button('Музыка Выкл', music_stop)
     menu.add_label('')
-    menu.add_text_input('Ваше имя : ', default='someone name')
     menu.add_label('')
-    menu.add_selector("Локация : ", [('название карты 1', 1), ('название карты 2', 2), ('название карты 3', 2)],
-                      onchange=set_difficulty)
-    menu.add_button('Играть', start_the_game)
+    menu.add_button('Играть', start_game)
+    menu.add_label('')
     menu.add_button('Выйти', pygame_menu.events.EXIT)
 
 
