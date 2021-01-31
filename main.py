5262c1f49243a1ffd0971a424f1307f31b11b5b0
ForestGameMain <Forest_game_channele@mail.ru>
2021-01-31 19:47:20 +0300

    Update Betta Version Start menu. Reworking music, up design

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame_menu
 
 pygame.init()
 surface = pygame.display.set_mode((1280, 720))
+song_start = pygame.mixer.Sound('Sounds/8bitlong.mp3')
 
 
 class StartPage():
@@ -12,25 +13,26 @@ class StartPage():
         pass
 
     def start_the_game():
+        print("START")
         # Сюда писать поссылки на саму игру внутри или сам код
         pass
 
-    def music_off_on(value, difficulty):
-        if value == 1:
-            pygame.mixer.unpause()
-        elif value == 0:
-            pygame.mixer.pause()
+    def music_play():
+        song_start.play()
 
-    menu = pygame_menu.Menu(720, 1280, 'Название игры', theme=pygame_menu.themes.THEME_DARK)
-    menu.add_selector("Музыка : ", [('Вкл', 1), ('Выкл', 0)], onchange=music_off_on)
-    menu.add_text_input('Ваше имя :', default='someone name')
-    menu.add_selector("Локация :", [('название карты 1', 1), ('название карты 2', 2), ('название карты 3', 2)],
+    def music_stop():
+        song_start.stop()
+
+    menu = pygame_menu.Menu(720, 1280, 'Arcanoid', theme=pygame_menu.themes.THEME_DARK)
+    menu.add_button('Музыка Вкл', music_play)
+    menu.add_button('Музыка Выкл', music_stop)
+    menu.add_label('')
+    menu.add_text_input('Ваше имя : ', default='someone name')
+    menu.add_label('')
+    menu.add_selector("Локация : ", [('название карты 1', 1), ('название карты 2', 2), ('название карты 3', 2)],
                       onchange=set_difficulty)
     menu.add_button('Играть', start_the_game)
     menu.add_button('Выйти', pygame_menu.events.EXIT)
-
-    song = pygame.mixer.Sound('Sounds/8bitlong.mp3')
-    song.play()
 
 
 if __name__ == '__main__':
